1913a7ee9907bafc9372a32f5363813e8166b673
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-30 02:39:21 +0900

    CheckNumpyInstall

---
+++ untitled/MachineRunningBasic/CheckNumpyInstall.py
@@ -0,0 +1,15 @@
+
+
+try:
+    import numpy
+    print("Numpt is installed")
+except:
+    print("nope")
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:fa7271/MachineRunningBasic.git
+git push -u origin main
