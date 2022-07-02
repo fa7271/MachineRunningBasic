b7564b20f0ab5e3f9b2fa32a7fdc92795327afb5
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-07-02 23:44:21 +0900

    220702 23:44

---
+++ untitled/MachineRunningBasic/1/RandomArray.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# 시각화 하는 라이브러리
+import matplotlib.pyplot as plt
+
+# np.random.normal(평균, 표준편차, 사이즈) # 정규분포 확률분포에서 확률 뽑음
+arr = np.random.normal(0, 1, (2, 3))
+print(arr)
+
+arr1 = np.random.normal(0, 1, 10000)
+plt.hist(arr1, bins = 100 ) #bins = 난수를 몇개의 구간으로 구분해서 보여줄지
+plt.show()
+
+# rand  0~1 사이 값을 균등한 비율로 표본을 표출    인자 = 표본갯수
+
+arr2 = np.random.rand(1000)
+plt.hist(arr2, bins = 100)
+plt.show()
+
+# randn -1 부터 1사이 정규분포 표본 추출
+
+arr3 = np.random.randn(1000) #표본의 갯수 1000개
+plt.hist(arr3, bins=100)  # 구간 100개
+plt.show()
+
+# randint 랜덤한 정수만 출력  low 값 high 값 size
+arr4 = np.random.randint(low = 1, high = 5, size = (3, 4))
+plt.hist(arr4)
--- untitled/MachineRunningBasic/1/range.py
+++ untitled/MachineRunningBasic/1/range.py
@@ -1,1 +1,22 @@
 import numpy as np
+# 특정 범위의 값을 가지는 N차원 배열생성
+
+# 순차적 값 생성 (이름순,번호순)
+
+lst = list(range(5, 9, 2 ))
+print(lst)
+
+# === 시작,끝(미만),갭
+arr = np.arange(5, 9, 2) # == arr = np.arrange(stop = 9, step = 2, start = 5)
+print(arr)
+
+# 시작 끝 넘 0 부터 100(포함)까지 11개 간격으로
+arr1= np.linspace(0, 100, 10)
+print(arr1)
+
+#log space 지정된 범위에서 넘 갯수만큼 균등한 간격으로 데이터를 르고스페이스로 생성
+
+#밑이 2인
+arr2 = np.logspace(1, 10, 10, base=2)
+print(arr2)
+
